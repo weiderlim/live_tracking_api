66a64ffa0a4a78fd3e289bd6e32519e1ab779551
Lim Wei Der <limweider@Lims-MacBook-Pro.local>
2024-08-02 15:42:57 +0800

    Example code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello world')
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    data = {
+        "exchange": "binance", 
+        "PIC": "Jansen", 
+        "balance":12345
+    }
+    
+    return jsonify(data), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
