16620a1f719464fec7fded59f4983ec96d310acd
Adam-leaf <164289963+Adam-leaf@users.noreply.github.com>
2024-08-02 18:18:02 +0800

    Test IP

--- db_exchange.py
+++ db_exchange.py
@@ -17,10 +17,11 @@ def lambda_handler(event, context):
     function_to_run = event.get('function_to_run')
 
     # Owners and mapping to PIC
-    # acc_owners = ['A']
-    acc_owners = ['J', 'JM', 'JM2', 'VKEE', 'KS']
+    acc_owners = ['A']
+    #acc_owners = ['J', 'JM', 'JM2', 'VKEE', 'KS']
 
     pic = {
+        "A": "Test",
         "J": "Jansen",
         "VKEE": "Vkee",
         "JM": "Joshua Moh",
